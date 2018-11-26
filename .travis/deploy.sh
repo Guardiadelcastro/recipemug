3a8ab1a8400941b4c64becb2c7a61a6a429e5dc9
Alejandro Castro <guardiadelcastro@gmail.com>
2018-11-26 19:19:55 +0000

    added ssh conection to travis in droplet

---
+++ .travis/deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# print outputs and exit on first failure
+set -xe
+
+if [ $TRAVIS_BRANCH == "master" ] ; then
+
+    # setup ssh agent, git config and remote
+    eval "$(ssh-agent -s)"
+    ssh-add ~/.ssh/travis_rsa
+    git remote add deploy "travis@206.189.96.128:/var/www/new.recipemug.club"
+    git config user.name "travis"
+    git config user.email "travis@recipemug.club"
+
+    # commit compressed files and push it to remote
+    rm -f .gitignore
+    cp .travis/deployignore .gitignore
+    git add .
+    git status # debug
+    git commit -m "Deploy compressed files"
+    git push -f deploy HEAD:master
+
+else
+
+    echo "No deploy script for branch '$TRAVIS_BRANCH'"
+
+fi
