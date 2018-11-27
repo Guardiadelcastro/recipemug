547257ea2eaea9648f3ae6b018d5c220c3fab35b
Javier Perez <mojocanario7@gmail.com>
2018-11-27 23:51:00 +0000

    Algunos cambios

--- src/views/components/navBar.vue
+++ src/views/components/navBar.vue
@@ -1,3 +1,28 @@
 <template>
-    
+    <div class="web-nav">
+        <nav class="nav-bar">
+            <div class="newRecipe">
+                <a href=""></a>
+                <p>NEW</p>
+            </div>
+            <div class="userSettings">
+                <a href=""></a>
+                <p>ICON</p>
+            </div>
+        </nav>
+    </div>
 </template>
+
+<style>
+    .nav-bar{
+        display: grid;
+        grid-template-columns: repeat(10, 1fr);
+        grid-template-rows: 1fr;
+        grid-gap: 10px;
+        min-height: 50px;
+        justify-items: center;
+        align-items: center;
+        box-shadow: 1px 2px 4px 0 rgba(0,0,0,.3);
+    }
+</style>
+
