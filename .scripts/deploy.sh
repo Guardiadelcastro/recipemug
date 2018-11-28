856631afaaa47ecfa28af356c35b3c7876611b36
Alejandro Castro <guardiadelcastro@gmail.com>
2018-11-28 18:45:59 +0000

    trying something else

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -2,19 +2,25 @@
 set -xe
 
 if [ $TRAVIS_BRANCH == 'travis' ] ; then
-  eval "$(ssh-agent -s)"
-  ssh-add ~/.ssh/id_rsa
-
+  #eval "$(ssh-agent -s)"
+  #ssh-add ~/.ssh/id_rsa
+tar -czf package.tgz build && \
+scp package.tgz $REMOTE_USER@$REMOTE_HOST:$REMOTE_APP_DIR && \
+ssh $REMOTE_USER@$REMOTE_HOST 'bash -s' < ./scripts/untar.sh
   #cd public
   #git init
 
-  git remote add deploy "travis@206.189.96.128:/var/www/new.recipemug.club"
-  git config user.name "travis"
-  git config user.email "travis@recipemug.club"
+  #git remote add deploy "travis@206.189.96.128:/var/www/new.recipemug.club"
+  #git config user.name "travis"
+  #git config user.email "travis@recipemug.club"
 
-  git add .
-  git commit -m "travis deploy"
-  git push --force deploy travis
+  #git add .
+  #git commit -m "travis deploy"
+  #git push --force deploy travis
 else
   echo "Not deploying, since this branch isn't master."
 fi
+
+
+set -x
+
