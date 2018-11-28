b6ef7a5cae47d6b33aeb517974fe84b3df54c5c5
Alejandro Castro <guardiadelcastro@gmail.com>
2018-11-28 11:25:27 +0000

    changed script once again

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -1,29 +1,20 @@
 #!/bin/bash
-
-# print outputs and exit on first failure
 set -xe
 
-if [ $TRAVIS_BRANCH == "travis" ] ; then
+if [ $TRAVIS_BRANCH == 'master' ] ; then
+  eval "$(ssh-agent -s)"
+  ssh-add ~/.ssh/id_rsa
 
-    # setup ssh agent, git config and remote
-    eval "$(ssh-agent -s)"
-    ssh-add ~/.ssh/id_rsa
-    git remote add deploy "travis@206.189.96.128:/var/www/new.recipemug.club"
-    git config user.name "travis"
-    git config user.email "travis@recipemug.club"
+  #cd public
+  #git init
 
-    # commit compressed files and push it to remote
-    rm -f .gitignore
-    # cp .travis/deployignore .gitignore
-    git add .
-    git status # debug
-    git commit -m "Travis eploy compressed files"
-    git push -f deploy HEAD:master
+  git remote add deploy "travis@206.189.96.128:/var/www/new.recipemug.club"
+  git config user.name "travis"
+  git config user.email "travis@recipemug.club"
 
+  git add .
+  git commit -m "travis deploy"
+  git push --force deploy master
 else
-
-    echo "No deploy script for branch '$TRAVIS_BRANCH'"
-
+  echo "Not deploying, since this branch isn't master."
 fi
-
-!/bin/bash
