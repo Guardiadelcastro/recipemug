5411191fb4e2ae1e788eacd7acd8a83da2223733
Alejandro Castro <guardiadelcastro@gmail.com>
2018-11-29 11:58:48 +0000

    changed from travis branch to develop branch

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -7,7 +7,7 @@ tar -czf package.tgz build
 scp package.tgz $REMOTE_USER@$REMOTE_HOST:$REMOTE_PROD_DIR
 ssh $REMOTE_USER@$REMOTE_HOST 'bash -s' < ./.scripts/deploy-prod.sh
 
-elif [ $TRAVIS_BRANCH == "travis" ] ; then
+elif [ $TRAVIS_BRANCH == "develop" ] ; then
 
 tar -czf package.tgz build
 scp package.tgz $REMOTE_USER@$REMOTE_HOST:$REMOTE_DEV_DIR
