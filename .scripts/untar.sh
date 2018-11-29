46855a40f219b6e32fce2e43462625bc8660a091
Alejandro Castro <guardiadelcastro@gmail.com>
2018-11-29 10:50:55 +0000

    changed untar.sh

--- .scripts/untar.sh
+++ .scripts/untar.sh
@@ -4,7 +4,7 @@ set -x
 # export NODE_ENV=production
 # export NVM_BIN=$HOME/.nvm/versions/node/v6.9.0/bin
 
-cd $REMOTE_NEW_DIR
+cd /var/www/new.recipemug.club
 tar zxvf package.tgz -C .
 # mv build/package.json .
 # npm i
