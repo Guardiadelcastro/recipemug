309f9ffed78807707d62ecd11ae07386037dff61
Alejandro Castro <guardiadelcastro@gmail.com>
2018-11-29 11:34:51 +0000

    divided untar script

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -5,13 +5,13 @@ if [ $TRAVIS_BRANCH == 'master' ] ; then
 
 tar -czf package.tgz build
 scp package.tgz $REMOTE_USER@$REMOTE_HOST:$REMOTE_PROD_DIR
-ssh $REMOTE_USER@$REMOTE_HOST 'bash -s' < ./.scripts/untar.sh
+ssh $REMOTE_USER@$REMOTE_HOST 'bash -s' < ./.scripts/deploy-prod.sh
 
 elif [ $TRAVIS_BRANCH == "travis" ] ; then
 
 tar -czf package.tgz build
-scp package.tgz $REMOTE_USER@$REMOTE_HOST:$REMOTE_NEW_DIR
-ssh $REMOTE_USER@$REMOTE_HOST 'bash -s' < ./.scripts/untar.sh
+scp package.tgz $REMOTE_USER@$REMOTE_HOST:$REMOTE_DEV_DIR
+ssh $REMOTE_USER@$REMOTE_HOST 'bash -s' < ./.scripts/deploy-dev.sh
 
 else
   echo "Not deploying, since this branch isn't master."
