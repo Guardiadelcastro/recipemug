c3bbbe684addaea224214b259a4529e5f27d9896
Alejandro Castro <guardiadelcastro@gmail.com>
2018-12-13 20:31:33 +0000

    fixed route in index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import App from './App.vue';
-import store from './store';
+import store from './store/store';
 
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faUser, faPlusSquare } from '@fortawesome/free-solid-svg-icons';
