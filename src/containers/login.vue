29ced86eb562b09db1788198a9625fc12c0c90ce
Aythami Z. Martin Gonzalez <43406334+Aythamiza@users.noreply.github.com>
2018-12-16 17:29:37 +0000

    sdas

--- src/containers/login.vue
+++ src/containers/login.vue
@@ -1,1 +1,81 @@
 
+<template>
+  <div class="login">
+    <form class="form">
+      <h1>Login or Register</h1>
+      <p>
+        <label
+          for="login"
+        >
+          Email
+        </label>
+        <input
+          type="text"
+          name="login"
+          placeholder="Email"
+          required
+        >
+      </p>
+      <p>
+        <label
+          for="password"
+        >
+          Password
+        </label>
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          required
+        > 
+      </p>
+
+      <RouterLink to="/dashboard">
+        <button type="submit">
+          Login
+        </button>
+      </RouterLink>   
+    </form>
+  </div>
+</template>
+<script>
+
+export default {
+
+  methods: {
+    login1: function () {
+    
+    }
+  }
+}; 
+</script>
+
+
+<style lang="stylus" scoped>
+@import '../styles/variables';
+
+.form
+  background-color green
+  width 300px
+  margin 60px auto 30px
+  padding 10px
+  display flex
+  align-items center
+  justify-content center
+  flex-flow column nowrap
+  font-family $font-title
+
+.form input 
+  align-items center
+  justify-content center
+  flex-flow column nowrap
+  font-family $font-title
+
+.from button 
+   align-items center
+  justify-content center
+  flex-flow column nowrap
+  font-family $font-title
+</style>
+
+
