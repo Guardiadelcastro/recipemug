7ee5353718589ae1d32460d4475c9f9ba580fb93
Alejandro Castro <guardiadelcastro@gmail.com>
2018-12-18 22:48:30 +0000

    changed deploy rsync

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -5,7 +5,7 @@ if [ $TRAVIS_BRANCH == 'master' ] ; then
 
 tar -czf package.tgz build
 scp package.tgz $REMOTE_USER@$REMOTE_HOST:$REMOTE_PROD_DIR
-rsync -raz ./src/assets/ $REMOTE_USER@$REMOTE_HOST:$REMOTE_ASSETS_DIR
+rsync -raz src/assets/ $REMOTE_USER@$REMOTE_HOST:$REMOTE_ASSETS_DIR
 ssh $REMOTE_USER@$REMOTE_HOST 'bash -s' < ./.scripts/deploy-prod.sh
 
 elif [ $TRAVIS_BRANCH == "develop" ] ; then
