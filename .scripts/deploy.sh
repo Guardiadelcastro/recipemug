b786d4feaa0de6bc4657ec11e7c9231df6924f06
Alejandro Castro <guardiadelcastro@gmail.com>
2018-12-18 22:50:30 +0000

    fixed deploy

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -12,7 +12,7 @@ elif [ $TRAVIS_BRANCH == "develop" ] ; then
 
 tar -czf package.tgz build
 scp package.tgz $REMOTE_USER@$REMOTE_HOST:$REMOTE_DEV_DIR
-rsync -raz ./src/assets $REMOTE_USER@$REMOTE_HOST:$REMOTE_ASSETS_DIR
+rsync -raz src/assets $REMOTE_USER@$REMOTE_HOST:$REMOTE_ASSETS_DIR
 ssh $REMOTE_USER@$REMOTE_HOST 'bash -s' < ./.scripts/deploy-dev.sh
 
 else
