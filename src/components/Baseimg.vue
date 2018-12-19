6a8ffce77b5f2a668932ece67b776d8d35964408
Aythami Z. Martin Gonzalez <43406334+Aythamiza@users.noreply.github.com>
2018-12-19 09:21:23 +0000

    baseimg

---
+++ src/components/Baseimg.vue
@@ -0,0 +1,30 @@
+<template>
+  <Img:
+    src="getpic(index)"
+    :alt="pic"
+  >
+    {{ getPic(index) }}
+  </img:>
+</template>
+
+<script>
+export default {
+  name:'Baseimg',
+  props: {
+    index: {
+      type: String,
+      default: ''
+    }
+  },
+  methods: {
+    getPic(index) {
+      return '../assets/' + this.pic[index] + '.png';
+    }
+  }
+
+};
+
+  
+
+</script>
+
