8845c3e87d9ed4b810d5d617cda9114f9df189ad
Alejandro Castro <guardiadelcastro@gmail.com>
2018-12-19 12:02:42 +0000

    week plan

---
+++ src/containers/WeekPlanCard.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="week-card">
+    <p class="day">
+      {{ day }}
+    </p>
+    <div class="recipe">
+      Add your recipe
+    </div>
+    <BaseButton
+      theme="green"
+      show-icon
+      icon="fas fa-chevron-circle-down"
+      name="Add Recipe"
+    />
+  </div>
+</template>
+
+<script>
+import BaseButton from '../components/BaseButton.vue';
+
+export default {
+  name: 'WeekPlanCard',
+  components:{
+    BaseButton
+  },
+  props:{
+    day: {
+      type: String,
+      default: ''
+    }
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+  @import '../styles/variables'
+
+  .week-card
+    width 200px
+    height 500px
+
+  .day
+    font-family $font
+    font-size 20px
+    color $grey
+  
+  .recipe
+    width 200px
+    height 200px
+    border 2px dashed $grey
+
+
+</style>
--- src/templates/WeekPlan.vue
+++ src/templates/WeekPlan.vue
@@ -1,12 +1,33 @@
 <template>
-  <h1 class="week-plan">
-    Week Plan
-  </h1>
+  <div class="week-plan">
+    <h1 class="week-plan">
+      Week Plan
+    </h1>
+    <WeekPlanCard />
+  </div>
 </template>
 
 <script>
-export default {
+import WeekPlanCard from '../containers/WeekPlanCard.vue';
 
+export default {
+  name:'WeekPlan',
+  components: {
+    WeekPlanCard
+  },
+  data() {
+    return {
+      days: [
+        {id: 1, day: 'Monday'},
+        {id: 2, day: 'Tuesday'},
+        {id: 3, day: 'Wednesday'},
+        {id: 4, day: 'Thursday'},
+        {id: 5, day: 'Friday'},
+        {id: 6, day: 'Saturday'},
+        {id: 7, day: 'Sunday'}
+      ]
+    };
+  }
 };
 </script>
 
