87b953d23ff5f388b9b0e0581a3ac278f47e95e4
Alejandro Castro <guardiadelcastro@gmail.com>
2018-12-19 12:07:33 +0000

    changed deploy

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -11,9 +11,9 @@ ssh $REMOTE_USER@$REMOTE_HOST 'bash -s' < ./.scripts/deploy-prod.sh
 elif [ $TRAVIS_BRANCH == "develop" ] ; then
 
 tar -czf package.tgz build
-rsync build/ $REMOTE_USER@$REMOTE_HOST:$REMOTE_DEV_DIR
+scp package.tgz $REMOTE_USER@$REMOTE_HOST:$REMOTE_DEV_DIR
 rsync -rauz src/assets/ $REMOTE_USER@$REMOTE_HOST:$REMOTE_ASSETS_DIR
-# ssh $REMOTE_USER@$REMOTE_HOST 'bash -s' < ./.scripts/deploy-dev.sh
+ssh $REMOTE_USER@$REMOTE_HOST 'bash -s' < ./.scripts/deploy-dev.sh
 
 else
   echo "Not deploying, since this branch isn't master."
