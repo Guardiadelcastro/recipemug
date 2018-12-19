603db4b7c1d384189c2adeae534e774dd73901ba
Aythami Z. Martin Gonzalez <43406334+Aythamiza@users.noreply.github.com>
2018-12-19 10:18:23 +0000

    recipe img

---
+++ src/containers/RecipeImg.vue
@@ -0,0 +1,66 @@
+<template>
+  <div
+    class="container"
+    @mouseenter="showButtons"
+    @mouseleave="removeButtons"
+  >
+    <div class="content">
+      <Img:
+        hrf="getpic(index)"
+        :alt="pic"
+      >
+        {{ getPic(index) }}
+      </img:>
+    </div>
+    <div class="buttons">
+      <BaseButton
+        v-show="show"
+        v-if="edit"
+        show-icon
+        icon="fas fa-edit"
+        theme="circle blue"
+        @click="editElement"
+      />
+      <BaseButton
+        v-else
+        show-icon
+        icon="fas fa-save"
+        theme="circle red"
+        @click="saveElement"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'Baseimg',
+  props: {
+    index: {
+      type: String,
+      default: ''
+    }
+  },
+  methods: {
+    getPic(index) {
+      return 'https://assets.recipemug.club/img/recipes/' + this.pic[index] + '.png';
+    },
+    showButtons() {
+      this.show = true;
+    },
+    removeButtons() {
+      this.show = false;
+    },
+    editElement() {
+      this.edit = false;
+    },
+    saveElement() {
+      this.edit = true;
+    }
+  }
+};
+
+  
+
+</script>
+
