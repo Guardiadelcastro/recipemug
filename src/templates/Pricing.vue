493a287e6eac4d99d22bdf6093b6f5e9a0fc4ff8
Javier Perez <mojocanario7@gmail.com>
2018-12-20 15:19:58 +0000

    Grid changes

--- src/templates/Pricing.vue
+++ src/templates/Pricing.vue
@@ -1,13 +1,30 @@
 <template>
-  <h1>Pricing</h1>
+  <nav class="pricing">
+    <div class="title">
+      <p class="text">
+        Pricing
+      </p>
+    </div>
+  </nav>
 </template>
 
 <script>
 export default {
-
+  name:'Pricing',
+  props: {}
 };
 </script>
 
-<style>
+<style lang="stylus" scoped>
+@import '../styles/variables'
+
+  .pricing
+    display grid
+    grid-template-columns 40% 10% 10% 40%
+    grid-template-rows 15% 85%
+
+  .title
+    grid-column 2 / 4
+    grid-row 1 / 2
 
 </style>
