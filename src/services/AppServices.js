8c8b824e6655413e23da7724a1bec8bf6998e699
Alejandro Castro <guardiadelcastro@gmail.com>
2019-04-23 11:27:45 +0100

    changed api

--- src/services/AppServices.js
+++ src/services/AppServices.js
@@ -1,4 +1,4 @@
 export const keys = {
-  apiUrl: process.env.LOCAL_API || 'http://localhost:3000'
-  // apiUrl: process.env.LOCAL_API || 'https://api.recipemug.club'
+  // apiUrl: process.env.LOCAL_API || 'http://localhost:3000'
+  apiUrl: process.env.LOCAL_API || 'https://api.recipemug.club'
 };
